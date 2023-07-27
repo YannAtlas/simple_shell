52c2a8d3d98af665b83f51f1b626717b08c2b9fb
falachabt <bennytenezeu@gmail.com>
2023-07-27 06:03:27 -0700

    yes

---
+++ execute.c
@@ -0,0 +1,6 @@
+#include "shell.h"
+
+int execute_command(char *command) {
+
+	return (0);
+}
---
+++ shell.c
@@ -0,0 +1,18 @@
+#include "shell.h"
+
+/**
+ * main - the main funct_ion of the code
+ *
+ * Return: nothinh
+ */
+int main(void)
+{
+	int i =  0;
+
+	while (i)
+	{
+	}
+
+	return (EXIT_SUCCESS);
+}
+
